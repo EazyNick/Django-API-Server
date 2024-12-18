3e4380713603a6cd25db5f3da42e87684ba374b0
Nick <kkkygsos@naver.com>
2024-12-18 15:49:00 +0900

    vote 후 결과 화면으로 이동
    vote 후 결과 화면으로 이동
    1. result url 추가
    2. view에 result 추가, 각 질문에 대한 답변이 몇개 투표됐는지 확인 가능

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -65,6 +65,21 @@ def detail(request, question_id):
     
 from django.db.models import F
 
+# def vote(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         return render(request, 'polls/detail.html', {'question': question, 'error_message': f"선택이 없습니다. id={request.POST['choice']}"})
+#     else:
+#         # A서버에서도 Votes = 1
+#         # B서버에서도 Votes = 1
+#         # A,B 서버에서 동시에 무언가를 투표할 경우, +2가 아닌 +1이 될 수 있으니 아래처럼 DB자체에서 카운트하게 수정
+#         # F는 DB에서의 값을 사용하라는 것
+#         selected_choice.votes = F('votes') + 1
+#         selected_choice.save()
+#         return HttpResponseRedirect(reverse('polls:index'))
+    
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -78,4 +93,10 @@ def vote(request, question_id):
         # F는 DB에서의 값을 사용하라는 것
         selected_choice.votes = F('votes') + 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:index'))
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
+    
+def result(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question': question})
+    
+    
