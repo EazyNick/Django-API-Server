5f6092ddb14cbc946bf88fb0319040f9157e1046
Nick <kkkygsos@naver.com>
2024-12-18 14:55:04 +0900

    상세(detail) 페이지로 링크 추가하기
    1. 문장 클릭 시 상세 페이지로 이동하는 기능
    2. html에서 url 참조하는 방법 학습

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# % url 'polls:detail' question.id % 처럼, 나중에 detail 이라는 이름의 url이 많아질 수 있으니, polls의 detail이라는 것을 명시하기 위해 사용
+app_name = 'polls'
+
 urlpatterns = [
     path('',views.index, name='index'), #polls 외에 파라미터로 아무것도 안붙어서 들어올 경우
     path('some_url',views.some_url),
