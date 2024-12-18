b5f04d033f992ad1c92c600bdcedb216d9f94d6d
Nick <kkkygsos@naver.com>
2024-12-18 14:41:00 +0900

    템플릿(Templates)에서 제어문 사용하기
    템플릿(Templates)에서 제어문 사용하기
    =제어문을 이용해 html에서 질문 여러개 화면에 출력

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,9 +5,15 @@ from django.shortcuts import render
 # def index(request):
 #     return HttpResponse("Hello, world.")
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5] # 최근 질문 5개 가져오기
+#     context = {'first_question': latest_question_list[0]} # 첫번쨰 질문 1개
+#     return render(request, 'polls/index.html', context)
+
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5] # 최근 질문 5개 가져오기
-    context = {'first_question': latest_question_list[0]} # 첫번쨰 질문 1개
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'questions': latest_question_list}
+    #context = {'questions': []}
     return render(request, 'polls/index.html', context)
 
 def some_url(request):
