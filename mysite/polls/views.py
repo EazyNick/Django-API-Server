c961377fbf7759caec7be7f385a6b354b1871e62
Nick <kkkygsos@naver.com>
2024-12-18 15:00:47 +0900

    404 에러 처리하기
    요청이 이상할 경우 500에러가 아닌, 404 에러가 나오게 해야함

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -19,6 +19,22 @@ def index(request):
 def some_url(request):
     return HttpResponse("Some ulr을 구현해 봤습니다.")
 
+# def detail(request, question_id):
+#     question = Question.objects.get(pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render , get_object_or_404
+
+...
 def detail(request, question_id):
-    question = Question.objects.get(pk=question_id)
+    """
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    """
+    # 위 코드를 간략화 하는 방법임 - get_object_or_404
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
