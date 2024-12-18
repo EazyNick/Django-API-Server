6159e38c8167734d1aebf9ef11217d2b7336f323
Nick <kkkygsos@naver.com>
2024-12-18 15:57:41 +0900

    Django Admin의 편집 페이지 커스터마이징
    1. admin 페이지에서 inlines를 통해 질문 항목에서 답변 목록까지 수정 가능
    2. pub_date는 자동으로 등록되니, 읽기전용으로 해야 에러안남
    3. admin.TabularInline 상속에 따라 답변항목들이 어떻게 나열되는지 다름

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import *
 
-#Register your models here
-admin.site.register(Question)
+# #Register your models here
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
 admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('질문 섹션', {'fields': ['question_text']}),
+        # 'classes': ['collapse']}로 숨기기 기능 가능
+        ('생성일', {'fields': ['pub_date'], 'classes': ['collapse']}),        
+    ]
+    # pub_date는 auto 등록 해놨기 때문에, 읽기전용으로 해야 에러가 안난다.
+    readonly_fields = ['pub_date']
+    # 질문 항목에서도 답변 항목을 수정 가능하게 함
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
