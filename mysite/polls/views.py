0bfb3618c76ec3776693baaf7fe4cd028df22c9b
Nick <kkkygsos@naver.com>
2024-12-18 14:12:36 +0900

    뷰(Views)와 템플릿(Templates)
    view에 index함수 정의해서 html 템플릿에 request, context를 전달해서 띄운다.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from .models import *
+from django.shortcuts import render
+
+# def index(request):
+#     return HttpResponse("Hello, world.")
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5] # 최근 질문 5개 가져오기
+    context = {'first_question': latest_question_list[0]} # 첫번쨰 질문 1개
+    return render(request, 'polls/index.html', context)
 
 def some_url(request):
     return HttpResponse("Some ulr을 구현해 봤습니다.")
