dfd3f8b186846f9195dbe7d1961f9671b73dffab
Nick <kkkygsos@naver.com>
2024-12-23 15:43:39 +0900

    Mixin
    더 간결하게 view 구현이 가능한 장점.

--- mysite/polls_api/urls.py
+++ mysite/polls_api/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # path('question/', QuestionList.as_view(), name='question-list'),
+    # path('question/<int:pk>/', QuestionDetail.as_view(), name='question-detail'),
     path('question/', QuestionList.as_view(), name='question-list'),
     path('question/<int:id>/', QuestionDetail.as_view(), name='question-detail'),
     # path('question/', question_list, name='question-list'),
