12c11c84d6fc44249550529d1607e9359454b868
Nick <kkkygsos@naver.com>
2024-12-23 15:38:51 +0900

    Class 기반의 뷰(Views)
    데코레이터 대신, 클래스를 활용해 api view 만들기
    데코레이터 대신 함수를 사용할뿐, 내용은 동일, urls에 .as_view()만 붙여주자.

--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -4,44 +4,79 @@ from polls_api.serializers import QuestionSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
-@api_view(['GET','POST'])  # http요청 허용, 지정하지 않았으니 모든 http 요청을 허용(get, post 등)
-def question_list(request):
-    """
-    API 엔드포인트로 동작하는 함수
-    """
-    if request.method == 'GET': # 모든 Question 목록 조회
+# Class 기반의 뷰(Views)
+class QuestionList(APIView):
+    def get(self, request):
         questions = Question.objects.all()
-        serializer = QuestionSerializer(questions, many = True) # json으로 변환(직렬화)
-        return Response(serializer.data) # 클라이언트에 JSON 형식의 응답을 반환
-    
-    if request.method == 'POST': # Questiong 등록
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
         serializer = QuestionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 에러기 때문에 400으로 해줘야함.
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def question_detail(request, id):
-    question = get_object_or_404(Question, pk=id)
-    
-    if request.method == 'GET':
+class QuestionDetail(APIView):
+    def get(self, request, id):
+        question = get_object_or_404(Question, pk=id)
         serializer = QuestionSerializer(question)
         return Response(serializer.data)
 
-    if request.method == 'PUT':
+    def put(self, request, id):
+        question = get_object_or_404(Question, pk=id)
         serializer = QuestionSerializer(question, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:    
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    if request.method == 'DELETE':
+        
+    def delete(self, request, id):
+        question = get_object_or_404(Question, pk=id)
         question.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# @api_view(['GET','POST'])  # http요청 허용, 지정하지 않았으니 모든 http 요청을 허용(get, post 등)
+# def question_list(request):
+#     """
+#     API 엔드포인트로 동작하는 함수
+#     """
+#     if request.method == 'GET': # 모든 Question 목록 조회
+#         questions = Question.objects.all()
+#         serializer = QuestionSerializer(questions, many = True) # json으로 변환(직렬화)
+#         return Response(serializer.data) # 클라이언트에 JSON 형식의 응답을 반환
+    
+#     if request.method == 'POST': # Questiong 등록
+#         serializer = QuestionSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # 에러기 때문에 400으로 해줘야함.
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def question_detail(request, id):
+#     question = get_object_or_404(Question, pk=id)
+    
+#     if request.method == 'GET':
+#         serializer = QuestionSerializer(question)
+#         return Response(serializer.data)
+
+#     if request.method == 'PUT':
+#         serializer = QuestionSerializer(question, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         else:    
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     if request.method == 'DELETE':
+#         question.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 """
 데이터 생성(Create) : POST
