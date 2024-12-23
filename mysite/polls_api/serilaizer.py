2a6bb5b7e369f1abbab7195599ba8a84938bda40
Nick <kkkygsos@naver.com>
2024-12-23 11:22:49 +0900

    Serilaizer
    서버와 클라이언트가 통신하기 위해(api 사용을 위해) 모델(데이터) -> Json 형식으로 변환
    1. create
    - 인스턴스 하나 만들기
    2. update
    - 검증된 값 있으면 그 값으로, 없으면 기존 값으로 업데이트

---
+++ mysite/polls_api/serilaizer.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from polls.models import Question
+
+class QuestionSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True) # 클라이언트가 데이터를 제공할 수 없게 함(read_only)
+    question_text = serializers.CharField(max_length=200)
+    pub_date = serializers.DateTimeField(read_only=True)
+
+    # validated_data는 입력된 데이터가 유효성 검사를 통과한 후 제공됨
+    def create(self, validated_data):
+        return Question.objects.create(**validated_data)
+
+    # instance는 기존의 Question 모델 인스턴스
+    def update(self, instance, validated_data):
+        instance.question_text = validated_data.get('question_text', instance.question_text)
+        instance.save()
+        return instance
