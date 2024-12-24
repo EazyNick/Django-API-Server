68049d697fba165a3eaca5f020f04d86904bc53d
Nick <kkkygsos@naver.com>
2024-12-24 11:56:48 +0900

    Testing Serializers
    1. setUp함수는 각 테스트가 실행되기 전에 실행이 된다.
    2. 테스트는 test_함수명 으로 작성해주면 된다.
    3. setup은 각 테스트마다 초기화되서 실행된다.

--- mysite/polls_api/tests.py
+++ mysite/polls_api/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from polls_api.serializers import QuestionSerializer
+from polls_api.serializers import QuestionSerializer, VoteSerializer
+from django.contrib.auth.models import User
+from polls.models import Question, Choice, Vote
 
 # python manage.py test
 
@@ -13,3 +15,64 @@ class QuestionSerializerTestCase(TestCase):
     def test_with_invalid_data(self):
         serializer = QuestionSerializer(data={'question_text': ''})
         self.assertEqual(serializer.is_valid(), False)
+
+class VoteSerializerTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser')
+        self.question = Question.objects.create(
+            question_text='abc',
+            owner=self.user,
+        )
+        self.choice = Choice.objects.create(
+            question=self.question
+            choice_text='1'
+        )    
+    
+   def test_vote_serializer(self):  
+        self.assertEqual(User.objects.all().count(), 1)
+        data = {
+            'question': self.question.id,
+            'choice': self.choice.id,
+            'voter': self.user.id
+        }
+        serializer = VoteSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        vote = serializer.save()
+    
+        self.assertEqual(vote.question, self.question)
+        self.assertEqual(vote.choice, self.choice)
+        self.assertEqual(vote.voter, self.user)
+    
+    def test_vote_serializer_with_duplicate_vote(self):
+        self.assertEqual(User.objects.all().count, 1)
+        choice1 = Choice.objects.create(
+            quetsion=self.question,
+            choice_text='2'
+        )
+        Vote.objects.create(question=self.question, choice=self.choice, voter=self.user)
+    
+        data = {
+            'question': self.question.id,
+            'choice': self.choice.id,
+            'voter': self.user.id
+        }
+        serializer = VoteSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+    
+    def test_vote_serilaizer_with_unmatched_question_and_choice(self):
+        question2 = Question.objects.create(
+            question_text='abc',
+            owner=self.user,
+        )
+    
+        choice2 = Choice.objects.create(
+            quetsion=question2,
+            choice_text='1'
+        )
+        data = {
+            'question': self.question.id,
+            'choice': self.choice.id,
+            'voter': self.user.id
+        }
+        serializer = VoteSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
