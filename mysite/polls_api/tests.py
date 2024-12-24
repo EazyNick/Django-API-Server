4339842288ba159e6b1c7bd9fa4e37bd5393ec8f
Nick <kkkygsos@naver.com>
2024-12-24 11:43:22 +0900

    python manage.py test
    python manage.py test로 실행해보면 test결과가 나온다.

--- mysite/polls_api/tests.py
+++ mysite/polls_api/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from polls_api.serializers import QuestionSerializer
 
+# python manage.py test
+
 class QuestionSerializerTestCase(TestCase):
     def test_with_valid_data(self):
         serializer = QuestionSerializer(data={'question_text': 'abc'})
